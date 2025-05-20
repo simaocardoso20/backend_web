f3293c98512fceeb0d61a1e5acd06655cb14689a
simaocardoso20 <simaoprof@hotmail.com>
2025-05-20 01:24:41 +0100

    fim

--- js/PL01.js
+++ js/PL01.js
@@ -1,23 +1,53 @@
+
+// Ex01
 function multiply(num1,num2) {
     let total = num1 * num2;
-    return total;
+    return 'O resultado da multiplicação é: ' + total;
 }
 
 
+
+// Ex02
 function isShortsWeather(temperature) {
     if (temperature > 25) {
-        console.log('Verdadeiro');
+        return 'Verdadeiro';
     } else {
-        console.log('Falso');
+        return 'Falso';
     }
 }
 
+
+
+
+// Ex03
 function lastElement(array) {
       if (array.length === 0) {
-    return null;
+        return null;
       } else {
-  return array[array.length - 1];
+  return "O último elemento do array é: " + array[array.length - 1];
+
 }
-}   
+}
 
 
+
+// Ex04
+function sumArray(array) {
+    let sum = 0;
+        for (let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+    return 'A soma de todos os números é: ' + sum; 
+}
+
+
+
+
+// Ex05
+function returnDay(dayNum) {
+    if(dayNum < 1 || dayNum > 7) {
+        return 'Null';
+    }
+    const days = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
+    return 'O dia da semana é: ' +days[dayNum - 1];
+}
